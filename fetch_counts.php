93cb62e459bdc6df1d0873e0f41c12c01e2d9bb9
benNjogu <kamandemachafi@gmail.com>
2025-06-11 11:53:21 +0300

    Securing credentials

--- fetch_counts.php
+++ fetch_counts.php
@@ -1,5 +1,7 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "rpl_page");
+require_once 'config.php';
+
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
